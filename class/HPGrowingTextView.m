45dfef40cea439392e8f8e66eae2380ed76420b2
Jason Sadler <jason.sadler@tribehr.com>
2013-09-06 11:42:13 -0400

    Applying bug fix described at https://devforums.apple.com/message/879837#879837

--- class/HPGrowingTextView.m
+++ class/HPGrowingTextView.m
@@ -611,11 +611,17 @@
         frame.size.height -= fudgeFactor.height;
         frame.size.width -= fudgeFactor.width;
         
+        NSString *textToMeasure = internalTextView.text;
+        if ([textToMeasure hasSuffix:@"\n"])
+        {
+            textToMeasure = [NSString stringWithFormat:@"%@-",internalTextView.text];
+        }
+        
         NSDictionary *attributes = @{NSFontAttributeName: internalTextView.font};
-        CGRect size = [internalTextView.text boundingRectWithSize:CGSizeMake(CGRectGetWidth(frame), MAXFLOAT)
-                                         options:NSStringDrawingUsesLineFragmentOrigin
-                                      attributes:attributes
-                                         context:nil];
+        CGRect size = [textToMeasure boundingRectWithSize:CGSizeMake(CGRectGetWidth(frame), MAXFLOAT)
+                                                  options:NSStringDrawingUsesLineFragmentOrigin
+                                               attributes:attributes
+                                                  context:nil];
         
         return CGRectGetHeight(size) + fudgeFactor.height;
     }
@@ -624,6 +630,5 @@
         return self.internalTextView.contentSize.height;
     }
 }
-
 
 @end
